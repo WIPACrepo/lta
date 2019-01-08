bd1acae964c393fac222edb3968e8bc579b01f75
David Schultz <davids24@gmail.com>
2019-01-08 11:09:58 -0600

    update config to take default values

--- lta/config.py
+++ lta/config.py
@@ -2,15 +2,19 @@
 
 import os
 
+NA = (0,) # use a tuple so 'is' operator works correctly
 
 def from_environment(keys):
     if isinstance(keys, str):
-        keys = [keys]
-    if not isinstance(keys, list):
-        raise TypeError(f"keys: Expected list of strings")
-    config = {}
-    for key in keys:
-        if key not in os.environ:
+        keys = {keys: NA}
+    elif isinstance(keys, list):
+        keys = dict.fromkeys(keys, NA)
+    elif not isinstance(keys, dict):
+        raise TypeError(f"keys: Expected string, list or dict")
+    config = keys.copy()
+    for key in config:
+        if key in os.environ:
+            config[key] = os.environ[key]
+        elif config[key] is NA:
             raise OSError(f"Missing environment variable '{key}'")
-        config[key] = os.environ[key]
     return config
