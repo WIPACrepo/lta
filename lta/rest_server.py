54b2c44e8264bc73ddf37b7d8af348f3f6697359
David Schultz <davids24@gmail.com>
2019-01-11 12:01:02 -0600

    tranfer request api - first pass

--- lta/rest_server.py
+++ lta/rest_server.py
@@ -6,6 +6,7 @@ Run with `python -m lta.rest_server`.
 
 import logging
 from functools import wraps
+from uuid import uuid1
 
 import tornado.web
 from tornado.ioloop import IOLoop
@@ -19,6 +20,9 @@ from .config import from_environment
 EXPECTED_CONFIG = {
     'LTA_REST_URL': 'localhost',
     'LTA_REST_PORT': 8080,
+    'LTA_AUTH_SECRET': 'secret',
+    'LTA_AUTH_ISSUER': 'lta',
+    'LTA_AUTH_ALGORITHM': 'RS256',
 }
 
 def lta_auth(**_auth):
@@ -45,7 +49,7 @@ def lta_auth(**_auth):
 
             authorized = False
 
-            auth_role = self.auth_data.get('long-term-archive',{}).get('role',None)
+            auth_role = self.auth_data.get('long-term-archive', {}).get('role', None)
             if roles and auth_role in roles:
                 authorized = True
             else:
@@ -69,22 +73,74 @@ class MainHandler(BaseLTAHandler):
     def get(self):
         self.write({})
 
+class TransferRequestsHandler(BaseLTAHandler):
+    @lta_auth(roles=['admin', 'user', 'system'])
+    async def get(self):
+        ret = {
+            'results': list(self.db['TransferRequests'].values()),
+        }
+        self.write(ret)
+
+    @lta_auth(roles=['admin', 'user', 'system'])
+    async def post(self):
+        req = json_decode(self.request.body)
+        req['uuid'] = uuid1().hex
+        self.db['TransferRequests'][req['uuid']] = req
+        self.set_status(201)
+        self.write({'TransferRequest': req['uuid']})
+
+class TransferRequestSingleHandler(BaseLTAHandler):
+    @lta_auth(roles=['admin', 'user', 'system'])
+    async def get(self, request_id):
+        if request_id not in self.db['TransferRequests']:
+            raise tornado.web.HTTPError(404, reason="not found")
+        self.write(self.db['TransferRequests'][request_id])
+
+    @lta_auth(roles=['admin', 'user', 'system'])
+    async def patch(self, request_id):
+        if request_id not in self.db['TransferRequests']:
+            raise tornado.web.HTTPError(404, reason="not found")
+        req = json_decode(self.request.body)
+        if 'uuid' in req and req['uuid'] != request_id:
+            raise tornado.web.HTTPError(400, reason="bad request")
+        self.db['TransferRequests'][request_id].update(req)
+        self.write({})
+
+    @lta_auth(roles=['admin', 'user', 'system'])
+    async def delete(self, request_id):
+        if request_id in self.db['TransferRequests']:
+            del self.db['TransferRequests'][request_id]
+            self.set_status(204)
+
+class TransferRequestActionsPopHandler(BaseLTAHandler):
+    @lta_auth(roles=['system'])
+    async def post(self):
+        raise tornado.web.HTTPError(500, reason="not implemented")
+        self.write({})
+
 def start(debug=False):
     config = from_environment(EXPECTED_CONFIG)
     logger = logging.getLogger('lta.rest')
-    
+
     args = RestHandlerSetup({
-#        'auth':{
-#            'secret':'secret',
-#        },
+        'auth': {
+            'secret': config['LTA_AUTH_SECRET'],
+            'issuer': config['LTA_AUTH_ISSUER'],
+            'algorithm': config['LTA_AUTH_ALGORITHM'],
+        },
         'debug': debug
     })
-    args['db'] = {} # this could be a DB, but a dict works for now
+    # this could be a DB, but a dict works for now
+    args['db'] = {'TransferRequests': {}}
 
     server = RestServer(debug=debug)
-    server.add_route('/', MainHandler, args)
+    server.add_route(r'/', MainHandler, args)
+    server.add_route(r'/TransferRequests', TransferRequestsHandler, args)
+    server.add_route(r'/TransferRequests/(?P<request_id>\w+)', TransferRequestSingleHandler, args)
+    server.add_route(r'/TransferRequests/actions/pop', TransferRequestActionsPopHandler, args)
+
     server.startup(address=config['LTA_REST_URL'],
-                   port=config['LTA_REST_PORT'])
+                   port=int(config['LTA_REST_PORT']))
     return server
 
 if __name__ == '__main__':
--- tests/test_rest_server.py
+++ tests/test_rest_server.py
@@ -1,16 +1,22 @@
 import asyncio
 import pytest
+import logging
 
 from lta.rest_server import start
 from rest_tools.client import RestClient
+from rest_tools.server import Auth
 
 @pytest.fixture
 async def rest(monkeypatch):
     monkeypatch.setenv("LTA_REST_PORT", "8080")
+    monkeypatch.setenv("LTA_AUTH_SECRET", "secret")
+    monkeypatch.setenv("LTA_AUTH_ISSUER", "lta")
+    monkeypatch.setenv("LTA_AUTH_ALGORITHM", "HS512")
     s = start(debug=True)
-    def client(token=''):
-        return RestClient('http://localhost:8080', token=token,
-                          timeout=0.1, backoff=False)
+    a = Auth('secret', issuer='lta', algorithm='HS512')
+    def client(role='admin'):
+        t = a.create_token('foo', payload={'long-term-archive':{'role':role}})
+        return RestClient('http://localhost:8080', token=t, timeout=0.1, retries=0)
     yield client
     s.stop()
 
@@ -22,3 +28,37 @@ async def test_server_reachability(rest):
     r = rest()
     ret = await r.request('GET', '/')
     assert ret == {}
+
+
+@pytest.mark.asyncio
+async def test_transfer_request_crud(rest):
+    """
+    Check CRUD semantics for transfer requests.
+    """
+    r = rest()
+    request = {'foo': 'bar'}
+    ret = await r.request('POST', '/TransferRequests', request)
+    uuid = ret['TransferRequest']
+    assert uuid
+
+    ret = await r.request('GET', '/TransferRequests')
+    assert len(ret['results']) == 1
+
+    ret = await r.request('GET', f'/TransferRequests/{uuid}')
+    for k in request:
+        assert request[k] == ret[k]
+
+    request2 = {'bar': 2}
+    ret = await r.request('PATCH', f'/TransferRequests/{uuid}', request2)
+    assert ret == {}
+
+    ret = await r.request('DELETE', f'/TransferRequests/{uuid}')
+    assert ret == None
+
+    with pytest.raises(Exception):
+        await r.request('GET', f'/TransferRequests/{uuid}')
+    ret = await r.request('DELETE', f'/TransferRequests/{uuid}')
+    assert ret == None
+
+    ret = await r.request('GET', '/TransferRequests')
+    assert len(ret['results']) == 0
