0bd1793d171d0199e62de7130069b4277dec1452
David Schultz <davids24@gmail.com>
2019-01-30 13:44:19 -0600

    add mypy to setupenv

--- setupenv.sh
+++ setupenv.sh
@@ -3,4 +3,4 @@ unset PYTHONPATH
 virtualenv -p python3 env
 echo "unset PYTHONPATH" >> env/bin/activate
 . env/bin/activate
-pip install requests tornado prometheus_client flake8 flake8-docstrings pytest pytest-mock pytest-cov pytest-asyncio git+https://github.com/WIPACrepo/rest-tools
+pip install requests tornado prometheus_client mypy flake8 flake8-docstrings pytest pytest-mock pytest-cov pytest-asyncio git+https://github.com/WIPACrepo/rest-tools
