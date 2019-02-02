4ff79b28729b7e5ae1f27403c544e4c5400d4f12
Patrick Meade <blinkdog@protonmail.com>
2019-02-01 19:52:30 -0600

    Updated documentation and helper scripts for dependency management

--- setupenv.sh
+++ setupenv.sh
@@ -3,4 +3,6 @@ unset PYTHONPATH
 virtualenv -p python3 env
 echo "unset PYTHONPATH" >> env/bin/activate
 . env/bin/activate
-pip install requests tornado prometheus_client mypy flake8 flake8-docstrings pytest pytest-mock pytest-cov pytest-asyncio git+https://github.com/WIPACrepo/rest-tools
+pip install --upgrade pip
+pip install flake8 flake8-docstrings mypy prometheus_client pytest pytest-asyncio pytest-cov pytest-mock requests tornado wheel
+pip install git+https://github.com/WIPACrepo/rest-tools
