3a4b6b4dba2aee689a76ca289fde4abb0e2c05fe
Patrick Meade <blinkdog@protonmail.com>
2019-02-25 12:35:28 -0600

    Add support for /Bundles routes

--- setupenv.sh
+++ setupenv.sh
@@ -4,5 +4,4 @@ virtualenv -p python3 env
 echo "unset PYTHONPATH" >> env/bin/activate
 . env/bin/activate
 pip install --upgrade pip
-pip install git+https://github.com/WIPACrepo/rest-tools
 pip install -r requirements.txt
