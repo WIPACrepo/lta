6048bfbfbea525b2466c803da19a23bae6ae3167
Patrick Meade <blinkdog@protonmail.com>
2019-03-24 11:16:23 -0500

    Implemented ltacmd request command

---
+++ resources/lta_cmd.py
@@ -0,0 +1,167 @@
+"""
+Command line utility for Long Term Archive.
+
+Run with `python -m resources.lta_cmd $@`.
+"""
+
+import argparse
+import asyncio
+import json
+import sys
+from typing import Dict
+
+from rest_tools.client import RestClient  # type: ignore
+
+from lta.config import from_environment
+
+EXPECTED_CONFIG = {
+    'LTA_REST_TOKEN': None,
+    'LTA_REST_URL': None
+}
+
+
+async def display_config(args) -> int:
+    if args.json:
+        print_dict_as_pretty_json(args.config)
+    else:
+        for key in args.config:
+            print(f"{key}:\t\t{args.config[key]}")
+    return 0
+
+
+def print_dict_as_pretty_json(d: Dict) -> None:
+    print(json.dumps(d, indent=4, sort_keys=True))
+
+
+async def request_ls(args) -> int:
+    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
+    try:
+        response = await rc.request("GET", "/TransferRequests")
+        if args.json:
+            print_dict_as_pretty_json(response)
+        else:
+            results = response["results"]
+            for x in results:
+                short_id = x["uuid"][:7]
+                create_time = x["create_timestamp"]
+                source_cook = x["source"].split(":")
+                path = source_cook[1]
+                source = source_cook[0]
+                dests = [y.split(":")[0] for y in x["dest"]]
+                print(f"{short_id}\t{create_time}\t{path}\t{source} -> {dests}")
+    except Exception as e:
+        print(e)
+        return -1
+    return 0
+
+
+async def request_new(args) -> int:
+    # configure a RestClient from the environment
+    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
+    # get some stuff
+    source = args.source
+    dest = args.dest
+    path = args.path
+    # construct the TransferRequest body
+    request_body = {
+        "source": f"{source}:{path}",
+        "dest": [f"{x}:{path}" for x in dest],
+    }
+    try:
+        response = await rc.request("POST", "/TransferRequests", request_body)
+        uuid = response["TransferRequest"]
+        tr = await rc.request("GET", f"/TransferRequests/{uuid}")
+        if args.json:
+            print_dict_as_pretty_json(tr)
+        else:
+            print(f"{uuid} - {tr['create_timestamp']}\n{source} -> {[x for x in args.dest]}\n{path}")
+    except Exception as e:
+        print(e)
+        return -1
+    return 0
+
+
+async def request_status(args) -> int:
+    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
+    try:
+        response = await rc.request("GET", "/TransferRequests")
+        results = response["results"]
+        for x in results:
+            if x["uuid"].startswith(args.uuid):
+                if args.json:
+                    print_dict_as_pretty_json(x)
+                else:
+                    print(f"Claimed: {x['claimed']}")
+    except Exception as e:
+        print(e)
+        return -1
+    return 0
+
+
+async def main():
+    # configure the application from the environment
+    config = from_environment(EXPECTED_CONFIG)
+
+    # define our top-level argument parsing
+    parser = argparse.ArgumentParser(prog="ltacmd")
+    parser.add_argument("-v",
+                        "--verbose",
+                        help="display verbose output",
+                        action="store_true")
+    parser.set_defaults(config=config)
+    subparser = parser.add_subparsers(help='command help')
+
+    # define a subparser for the 'display-config' subcommand
+    parser_display_config = subparser.add_parser('display-config', help='display environment configuration')
+    parser_display_config.add_argument("--json",
+                                       help="display output in JSON",
+                                       action="store_true")
+    parser_display_config.set_defaults(func=display_config)
+
+    # define a subparser for the 'request' subcommand
+    parser_request = subparser.add_parser('request', help='interact with transfer requests')
+    request_subparser = parser_request.add_subparsers(help='request command help')
+
+    # define a subparser for the 'request ls' subcommand
+    parser_request_ls = request_subparser.add_parser('ls', help='list transfer requests')
+    parser_request_ls.add_argument("--json",
+                                   help="display output in JSON",
+                                   action="store_true")
+    parser_request_ls.set_defaults(func=request_ls)
+
+    # define a subparser for the 'request new' subcommand
+    parser_request_new = request_subparser.add_parser('new', help='create new transfer request')
+    parser_request_new.add_argument("--source",
+                                    help="source of files",
+                                    required=True)
+    parser_request_new.add_argument("--dest",
+                                    action="append",
+                                    help="destination of bundles",
+                                    required=True)
+    parser_request_new.add_argument("--path",
+                                    help="Data Warehouse path to be transferred",
+                                    required=True)
+    parser_request_new.add_argument("--json",
+                                    help="display output in JSON",
+                                    action="store_true")
+    parser_request_new.set_defaults(func=request_new)
+
+    # define a subparser for the 'request status' subcommand
+    parser_request_status = request_subparser.add_parser('status', help='query transfer request status')
+    parser_request_status.add_argument("--uuid",
+                                       help="identity of transfer request",
+                                       required=True)
+    parser_request_status.add_argument("--json",
+                                       help="display output in JSON",
+                                       action="store_true")
+    parser_request_status.set_defaults(func=request_status)
+
+    # parse the provided command line arguments and call the function
+    args = parser.parse_args()
+    exit_code = await args.func(args)
+    sys.exit(exit_code)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
