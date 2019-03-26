7929066189d1d451866dbe767143736c50f0c88b
Patrick Meade <blinkdog@protonmail.com>
2019-03-26 11:54:39 -0500

    Improvements to ltacmd, refactoring support scripts

--- lta-db.sh
+++ lta-db.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-export LTA_AUTH_ALGORITHM="HS256"
-export LTA_AUTH_ISSUER="lta"
-export LTA_AUTH_SECRET="$(<local-secret)"
-export LTA_MAX_CLAIM_AGE_HOURS="12"
-export LTA_REST_HOST="127.0.0.1"
-export LTA_REST_PORT="8080"
+export LTA_AUTH_ALGORITHM=${LTA_AUTH_ALGORITHM:="HS256"}
+export LTA_AUTH_ISSUER=${LTA_AUTH_ISSUER:="lta"}
+export LTA_AUTH_SECRET=${LTA_AUTH_SECRET:="$(<local-secret)"}
+export LTA_MAX_CLAIM_AGE_HOURS=${LTA_MAX_CLAIM_AGE_HOURS:="12"}
+export LTA_REST_HOST=${LTA_REST_HOST:="127.0.0.1"}
+export LTA_REST_PORT=${LTA_REST_PORT:="8080"}
 python -m lta.rest_server
--- resources/lta_cmd.py
+++ resources/lta_cmd.py
@@ -6,8 +6,9 @@ Run with `python -m resources.lta_cmd $@`.
 
 import argparse
 import asyncio
+from datetime import datetime, timedelta
 import json
-import sys
+import logging
 from typing import Dict
 
 from rest_tools.client import RestClient  # type: ignore
@@ -20,44 +21,41 @@ EXPECTED_CONFIG = {
 }
 
 
-async def display_config(args) -> int:
+async def display_config(args) -> None:
+    """Display the configuration provided to the application."""
     if args.json:
         print_dict_as_pretty_json(args.config)
     else:
         for key in args.config:
             print(f"{key}:\t\t{args.config[key]}")
-    return 0
 
 
 def print_dict_as_pretty_json(d: Dict) -> None:
+    """Print the provided Dict as pretty-print JSON."""
     print(json.dumps(d, indent=4, sort_keys=True))
 
 
-async def request_ls(args) -> int:
-    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
-    try:
-        response = await rc.request("GET", "/TransferRequests")
-        if args.json:
-            print_dict_as_pretty_json(response)
-        else:
-            results = response["results"]
-            for x in results:
-                short_id = x["uuid"][:7]
-                create_time = x["create_timestamp"]
-                source_cook = x["source"].split(":")
-                path = source_cook[1]
-                source = source_cook[0]
-                dests = [y.split(":")[0] for y in x["dest"]]
-                print(f"{short_id}\t{create_time}\t{path}\t{source} -> {dests}")
-    except Exception as e:
-        print(e)
-        return -1
-    return 0
+async def request_ls(args) -> None:
+    """List all of the TransferRequest objects in the LTA DB."""
+    response = await args.rc.request("GET", "/TransferRequests")
+    if args.json:
+        print_dict_as_pretty_json(response)
+    else:
+        results = response["results"]
+        for x in results:
+            display_id = x["uuid"]
+            if not args.long:
+                display_id = display_id[:8]
+            create_time = x["create_timestamp"].replace("T", " ")
+            source_cook = x["source"].split(":")
+            path = source_cook[1]
+            source = source_cook[0]
+            dests = [y.split(":")[0] for y in x["dest"]]
+            print(f"{display_id}  {create_time} {path} {source} -> {dests}")
 
 
-async def request_new(args) -> int:
-    # configure a RestClient from the environment
-    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
+async def request_new(args) -> None:
+    """Create a new TransferRequest and add it to the LTA DB."""
     # get some stuff
     source = args.source
     dest = args.dest
@@ -67,75 +65,82 @@ async def request_new(args) -> int:
         "source": f"{source}:{path}",
         "dest": [f"{x}:{path}" for x in dest],
     }
-    try:
-        response = await rc.request("POST", "/TransferRequests", request_body)
-        uuid = response["TransferRequest"]
-        tr = await rc.request("GET", f"/TransferRequests/{uuid}")
+    response = await args.rc.request("POST", "/TransferRequests", request_body)
+    uuid = response["TransferRequest"]
+    tr = await args.rc.request("GET", f"/TransferRequests/{uuid}")
+    if args.json:
+        print_dict_as_pretty_json(tr)
+    else:
+        display_id = tr["uuid"]
+        create_time = tr["create_timestamp"].replace("T", " ")
+        source_cook = tr["source"].split(":")
+        path = source_cook[1]
+        source = source_cook[0]
+        dests = [y.split(":")[0] for y in tr["dest"]]
+        print(f"{display_id}  {create_time} {path} {source} -> {dests}")
+
+
+async def request_status(args) -> None:
+    """Query the status of a TransferRequest in the LTA DB."""
+    response = await args.rc.request("GET", "/TransferRequests")
+    results = response["results"]
+    for x in results:
+        if x["uuid"].startswith(args.uuid):
+            if args.json:
+                print_dict_as_pretty_json(x)
+            else:
+                display_id = x["uuid"]
+                status_name = "Submitted - Waiting to be claimed"
+                status_time = x["create_timestamp"].replace("T", " ")
+                if x["claimed"]:
+                    status_name = "Claimed - Currently processing"
+                    status_time = x["claim_time"].replace("T", " ")
+                    if "complete" in x:
+                        status_name = "Completed - Files submitted to LTA DB"
+                        status_time = x["complete"]["timestamp"].replace("T", " ")
+                print(f"{display_id}  {status_time} {status_name}")
+
+
+async def status(args) -> None:
+    """Query the status of the LTA DB or a component of LTA."""
+    old_data = (datetime.utcnow() - timedelta(seconds=60*5)).isoformat()
+
+    def date_ok(d: str) -> bool:
+        return d > old_data
+
+    # if we want the status of a particular component type
+    if args.component:
+        response = await args.rc.request("GET", f"/status/{args.component}")
         if args.json:
-            print_dict_as_pretty_json(tr)
+            print_dict_as_pretty_json(response)
         else:
-            print(f"{uuid} - {tr['create_timestamp']}\n{source} -> {[x for x in args.dest]}\n{path}")
-    except Exception as e:
-        print(e)
-        return -1
-    return 0
-
-
-async def request_status(args) -> int:
-    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
-    try:
-        response = await rc.request("GET", "/TransferRequests")
-        results = response["results"]
-        for x in results:
-            if x["uuid"].startswith(args.uuid):
-                if args.json:
-                    print_dict_as_pretty_json(x)
-                else:
-                    print(f"Claimed: {x['claimed']}")
-    except Exception as e:
-        print(e)
-        return -1
-    return 0
-
-
-async def status(args) -> int:
-    rc = RestClient(args.config["LTA_REST_URL"], token=args.config["LTA_REST_TOKEN"])
-    try:
-        # if we want the status of a particular component type
-        if args.component:
-            response = await rc.request("GET", f"/status/{args.component}")
-            if args.json:
-                print_dict_as_pretty_json(response)
-            else:
-                for key in response:
-                    print(f"{(key+':'):<25}{response[key]['timestamp']}")
-        # otherwise we want the status of the whole system
+            for key in response:
+                timestamp = response[key]['timestamp']
+                status = "WARN"
+                if date_ok(timestamp):
+                    status = "OK"
+                print(f"{(key+':'):<25}[{status:<4}] {timestamp.replace('T', ' ')}")
+    # otherwise we want the status of the whole system
+    else:
+        response = await args.rc.request("GET", "/status")
+        if args.json:
+            print_dict_as_pretty_json(response)
         else:
-            response = await rc.request("GET", "/status")
-            if args.json:
-                print_dict_as_pretty_json(response)
-            else:
-                print(f"LTA:          {response['health']}")
-                for key in response:
-                    if key != "health":
-                        print(f"{(key+':'):<14}{response[key]}")
-    except Exception as e:
-        print(e)
-        return -1
-    return 0
+            print(f"LTA:          {response['health']}")
+            for key in response:
+                if key != "health":
+                    print(f"{(key+':'):<14}{response[key]}")
 
 
 async def main():
+    """Process a request from the Command Line."""
     # configure the application from the environment
     config = from_environment(EXPECTED_CONFIG)
+    rc = RestClient(config["LTA_REST_URL"], token=config["LTA_REST_TOKEN"])
 
     # define our top-level argument parsing
     parser = argparse.ArgumentParser(prog="ltacmd")
-    parser.add_argument("-v",
-                        "--verbose",
-                        help="display verbose output",
-                        action="store_true")
-    parser.set_defaults(config=config)
+    parser.set_defaults(config=config, rc=rc)
     subparser = parser.add_subparsers(help='command help')
 
     # define a subparser for the 'display-config' subcommand
@@ -153,6 +158,9 @@ async def main():
     parser_request_ls = request_subparser.add_parser('ls', help='list transfer requests')
     parser_request_ls.add_argument("--json",
                                    help="display output in JSON",
+                                   action="store_true")
+    parser_request_ls.add_argument("--long",
+                                   help="display long format UUIDs",
                                    action="store_true")
     parser_request_ls.set_defaults(func=request_ls)
 
@@ -196,10 +204,13 @@ async def main():
 
     # parse the provided command line arguments and call the function
     args = parser.parse_args()
-    exit_code = await args.func(args)
-    sys.exit(exit_code)
+    if hasattr(args, "func"):
+        await args.func(args)
+    else:
+        parser.print_usage()
 
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.CRITICAL)
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main())
