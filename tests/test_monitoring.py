b725c849bac83ef4be85745448c637b6b686e9e8
Patrick Meade <blinkdog@protonmail.com>
2019-03-26 15:01:46 -0500

    Gotta refactor the unit tests for monitoring as well

--- tests/test_monitoring.py
+++ tests/test_monitoring.py
@@ -2,27 +2,14 @@
 """Unit tests for lta/monitoring.py."""
 
 import asyncio
+import socket
+
 import pytest  # type: ignore
 import requests
-import socket
-from unittest.mock import MagicMock
 
 import lta.monitoring
+from .test_util import AsyncMock
 
-
-class AsyncMock(MagicMock):
-    """
-    AsyncMock is the async version of a MagicMock.
-
-    We use this class in place of MagicMock when we want to mock
-    asynchronous callables.
-
-    Source: https://stackoverflow.com/a/32498408
-    """
-
-    async def __call__(self, *args, **kwargs):
-        """Allow MagicMock to work its magic too."""
-        return super(AsyncMock, self).__call__(*args, **kwargs)
 
 @pytest.fixture
 def monitor(monkeypatch, mocker):
