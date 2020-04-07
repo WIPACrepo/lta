98dd9259af1c5526bc7e8fb47d65a099ee795efe
Patrick Meade <blinkdog@protonmail.com>
2020-04-07 15:07:23 -0500

    Normalize paths and whitelist prefixes

---
+++ tests/test_lta_cmd.py
@@ -0,0 +1,19 @@
+# test_lta_cmd.py
+"""Unit tests for lta/lta_cmd.py."""
+
+import pytest  # type: ignore
+
+from lta.lta_cmd import normalize_path
+
+
+def test_normalize_path() -> None:
+    """Test that normalize_path will normalize paths properly."""
+    assert normalize_path("/data/exp/IceCube/2018/unbiased/PFRaw/1109") == "/data/exp/IceCube/2018/unbiased/PFRaw/1109"
+    assert normalize_path("/data/exp/IceCube/2018/unbiased/PFRaw/1109/") == "/data/exp/IceCube/2018/unbiased/PFRaw/1109"
+    assert normalize_path("/data/exp/IceCube/2018/unbiased/PFRaw/1109/.") == "/data/exp/IceCube/2018/unbiased/PFRaw/1109"
+
+
+def test_PATH_PREFIX_WHITELIST() -> None:
+    """Test that normalize_path will enforce PATH_PREFIX_WHITELIST."""
+    with pytest.raises(ValueError):
+        normalize_path("/mnt/lfs7/exp/IceCube/2018/unbiased/PFRaw/1109")
