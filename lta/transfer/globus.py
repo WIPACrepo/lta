9f975da01573393e473144abeef22667dd14a099
Patrick Meade <blinkdog@protonmail.com>
2021-01-21 00:35:45 -0600

    Initial port and refactor of GridFTP support

---
+++ lta/transfer/globus.py
@@ -0,0 +1,90 @@
+# globus.py
+"""Tools to help manage Globus proxies."""
+
+import os
+import subprocess
+import logging
+from typing import Dict, Optional
+
+from rest_tools.server import from_environment  # type: ignore
+
+PROXY_CONFIG: Dict[str, Optional[str]] = {
+    "GLOBUS_PROXY_DURATION": "72",
+    "GLOBUS_PROXY_PASSPHRASE": None,
+    "GLOBUS_PROXY_VOMS_ROLE": "",
+    "GLOBUS_PROXY_VOMS_VO": "",
+    "GLOBUS_PROXY_OUTPUT": "",
+}
+
+logger = logging.getLogger('globus')
+
+class SiteGlobusProxy(object):
+    """
+    Manage site-wide globus proxy.
+
+    :param duration: proxy duration (optional, default 72 hours)
+    """
+
+    def __init__(self, duration: Optional[int] = None):
+        """Create a SiteGlobusProxy object."""
+        self.cfg = from_environment(PROXY_CONFIG)
+        if duration:
+            self.cfg['GLOBUS_PROXY_DURATION'] = duration
+
+    def set_duration(self, d):
+        """Set the duration."""
+        self.cfg['GLOBUS_PROXY_DURATION'] = d
+
+    def set_passphrase(self, p):
+        """Set the passphrase."""
+        self.cfg['GLOBUS_PROXY_PASSPHRASE'] = p
+
+    def set_voms_role(self, r):
+        """Set the voms role."""
+        self.cfg['GLOBUS_PROXY_VOMS_ROLE'] = r
+
+    def set_voms_vo(self, vo):
+        """Set the voms VO."""
+        self.cfg['GLOBUS_PROXY_VOMS_VO'] = vo
+
+    def update_proxy(self):
+        """Update the proxy."""
+        logger.info('duration: %r', self.cfg['GLOBUS_PROXY_DURATION'])
+        if subprocess.call(['grid-proxy-info', '-e', '-valid', f'{self.cfg["GLOBUS_PROXY_DURATION"]}:0'],
+                           stdout=subprocess.DEVNULL,
+                           stderr=subprocess.DEVNULL):
+            # proxy needs updating
+            if 'GLOBUS_PROXY_VOMS_VO' in self.cfg and self.cfg['GLOBUS_PROXY_VOMS_VO']:
+                cmd = ['voms-proxy-init']
+                if 'GLOBUS_PROXY_VOMS_ROLE' in self.cfg and self.cfg['GLOBUS_PROXY_VOMS_ROLE']:
+                    vo = self.cfg['GLOBUS_PROXY_VOMS_VO']
+                    role = self.cfg['GLOBUS_PROXY_VOMS_ROLE']
+                    cmd.extend(['-voms', '{0}:/{0}/Role={1}'.format(vo, role)])
+                else:
+                    cmd.extend(['-voms', self.cfg['GLOBUS_PROXY_VOMS_VO']])
+            else:
+                cmd = ['grid-proxy-init']
+            cmd.extend(['-pwstdin', '-valid', f'{self.cfg["GLOBUS_PROXY_DURATION"]+1}:0'])
+            if 'GLOBUS_PROXY_OUTPUT' in self.cfg and self.cfg['GLOBUS_PROXY_OUTPUT']:
+                cmd.extend(['-out', self.cfg['GLOBUS_PROXY_OUTPUT']])
+            inputbytes = (self.cfg['GLOBUS_PROXY_PASSPHRASE']+'\n').encode('utf-8')
+            p = subprocess.run(cmd, input=inputbytes, capture_output=True, timeout=60, check=False)
+            logger.info('proxy cmd: %r', p.args)
+            logger.info('stdout: %s', p.stdout)
+            logger.info('stderr: %s', p.stderr)
+            if 'GLOBUS_PROXY_VOMS_VO' in self.cfg and self.cfg['GLOBUS_PROXY_VOMS_VO']:
+                for line in p.stdout.decode('utf-8').split('\n'):
+                    if line.startswith('Creating proxy') and line.endswith('Done'):
+                        break  # this is a good proxy
+                else:
+                    raise Exception('voms-proxy-init failed')
+            elif p.returncode > 0:
+                raise Exception('grid-proxy-init failed')
+
+    def get_proxy(self):
+        """Get the proxy location."""
+        if 'GLOBUS_PROXY_OUTPUT' in self.cfg and self.cfg['GLOBUS_PROXY_OUTPUT']:
+            return self.cfg['GLOBUS_PROXY_OUTPUT']
+        FNULL = open(os.devnull, 'w')
+        return subprocess.check_output(['grid-proxy-info', '-path'],
+                                       stderr=FNULL).decode('utf-8').strip()
