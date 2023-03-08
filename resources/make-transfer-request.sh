163b5dac748329d88eaa44a58106168d48f1bbc3
Patrick Meade <blinkdog@protonmail.com>
2023-03-08 12:51:01 -0600

    Clean up README.md

--- resources/make-transfer-request.sh
+++ resources/make-transfer-request.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 export LTA_REST_URL=${LTA_REST_URL:="http://127.0.0.1:8080"}
-python -m resources.make_transfer_request $@
+python3 -m resources.make_transfer_request $@
--- resources/test-data-helper.sh
+++ resources/test-data-helper.sh
@@ -2,4 +2,4 @@
 export FAKE_CHECKSUM=${FAKE_CHECKSUM:="True"}
 export FILE_CATALOG_REST_URL=${FILE_CATALOG_REST_URL:="http://127.0.0.1:8889"}
 export LTA_REST_URL=${LTA_REST_URL:="http://127.0.0.1:8080"}
-python -m resources.test_data_helper $@
+python3 -m resources.test_data_helper $@
