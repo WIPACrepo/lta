cefbe9808acd48d4a7018abaacb70eab2f3398a6
Patrick Meade <blinkdog@protonmail.com>
2023-04-18 04:27:22 -0500

    Don't default SBATCH_PATH to SACCT_PATH

--- resources/nersc-controller.sh
+++ resources/nersc-controller.sh
@@ -10,7 +10,7 @@ export LOG_LEVEL=${LOG_LEVEL:="DEBUG"}
 export LOG_PATH=${LOG_PATH:="${HOME}/lta/nersc_controller.log"}
 export LTA_BIN_DIR=${LTA_BIN_DIR:="${HOME}/lta/bin"}
 export SACCT_PATH=${SACCT_PATH:="/usr/bin/sacct"}
-export SBATCH_PATH=${SACCT_PATH:="/usr/bin/sbatch"}
+export SBATCH_PATH=${SBATCH_PATH:="/usr/bin/sbatch"}
 export SLURM_LOG_DIR=${SLURM_LOG_DIR:="${HOME}/lta/slurm-logs"}
 export SQUEUE_PATH=${SQUEUE_PATH:="/usr/bin/squeue"}
 python3 -m resources.nersc_controller
