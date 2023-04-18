ba3a0964dc7e9e8c6b4c9e8c58ba4bb78fe7474a
Patrick Meade <blinkdog@protonmail.com>
2023-04-18 02:52:34 -0500

    Slurm commands are different on Perlmutter

--- resources/nersc-controller.sh
+++ resources/nersc-controller.sh
@@ -9,7 +9,7 @@ source env/bin/activate
 export LOG_LEVEL=${LOG_LEVEL:="DEBUG"}
 export LOG_PATH=${LOG_PATH:="${HOME}/lta/nersc_controller.log"}
 export LTA_BIN_DIR=${LTA_BIN_DIR:="${HOME}/lta/bin"}
-export SACCT_PATH=${SACCT_PATH:="/opt/esslurm/bin/sacct"}
-export SBATCH_PATH=${SACCT_PATH:="/opt/esslurm/bin/sbatch"}
+export SACCT_PATH=${SACCT_PATH:="/usr/bin/sacct"}
+export SBATCH_PATH=${SACCT_PATH:="/usr/bin/sbatch"}
 export SLURM_LOG_DIR=${SLURM_LOG_DIR:="${HOME}/lta/slurm-logs"}
 python3 -m resources.nersc_controller
