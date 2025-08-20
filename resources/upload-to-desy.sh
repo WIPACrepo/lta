4c4f65105ec9a5cbbbe7cea57398324eb0c70630
Patrick Meade <pmeade@icecube.wisc.edu>
2025-08-19 22:45:59 -0500

    Bump timeout to allow 10 minutes per GB

--- resources/upload-to-desy.sh
+++ resources/upload-to-desy.sh
@@ -5,7 +5,7 @@ export CLIENT_ID=${CLIENT_ID:="long-term-archive"}
 export CLIENT_SECRET=${CLIENT_SECRET:="$(<keycloak-client-secret)"}
 export DEST_BASE_PATH=${DEST_BASE_PATH:="/pnfs/ifh.de/acs/icecube/archive"}
 export DEST_URL=${DEST_URL:="https://globe-door.ifh.de:2880"}
-export LOG_LEVEL=${LOG_LEVEL:="DEBUG"}
+export LOG_LEVEL=${LOG_LEVEL:="INFO"}
 export LTA_AUTH_OPENID_URL=${LTA_AUTH_OPENID_URL:="https://keycloak.icecube.wisc.edu/auth/realms/IceCube"}
 export MAX_PARALLEL=${MAX_PARALLEL:="100"}
 export WORK_RETRIES=${WORK_RETRIES:="3"}
