2016ddd54110e5f170b2895514e18b050c98cc5f
Ric Evans <19216225+ric-evans@users.noreply.github.com>
2025-09-22 13:55:30 -0500

    Upgrade CI; Clean Up Root Dir [no-bump] (#319)
    Co-authored-by: github-actions <github-actions@github.com>

--- docker-deps-down.sh
+++
@@ -1,4 +0,0 @@
-# docker-deps-down.sh
-# Stop the docker containers that provide dev/test dependencies
-
-docker stop test-lta-mongo
--- lta/__init__.py
+++ lta/__init__.py
@@ -2,5 +2,5 @@
 
 from __future__ import absolute_import, division, print_function
 
-# NOTE: `__version__` is not defined as this package is built using 'setuptools-scm' --
-#   use `importlib.metadata.version(...)` if you need to access version info at runtime
+# NOTE: `__version__` is not defined because this package is built using 'setuptools-scm' --
+#   use `importlib.metadata.version(...)` if you need to access version info at runtime.
